7a677784edeeb92036fcc0a234cf45504180a78c
Gaurav Kumar <gauravsng1994@gmail.com>
2021-05-24 16:22:55 +0530

    bottom button fixed

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,7 +1,7 @@
 import BottomButton from "../BottomButton/index";
 import data from "../../data/quiz_config_v2_full.json";
 import Header from "../Header/index";
-import GirlImage from '../../assets/downloadedImages/you_go_girl_background.png';
+// import GirlImage from '../../assets/downloadedImages/you_go_girl_background.png';
 export default {
     name:"Categories",
     components:{
@@ -19,26 +19,21 @@ export default {
             categoryImage:'',
             categoryName:'',
             completedQuestionsCount : 0,
-            totalQuestionsCount : 0
+            totalQuestionsCount : 0,
+            selectedCategoryId:''
         };
     },
     mounted(){
         const sortedCategories = data.categories.sort( (a,b) => a.sequence - b.sequence);
         console.log('data',sortedCategories);
         this.headerTitle = (data||{}).title || "";
-    //     "id": "the_family",
-    //   "sequence": "1",
-    //   "title": "The Family",
-    //   "not_chosen_icon": "https://thekardashians.b-cdn.net/the_family/icons/catagory_icon_not_chosen_the_family.svg",
-    //   "chosen_icon": "https://thekardashians.b-cdn.net/the_family/icons/catagory_icon_chosen_the_family.svg",
-    //   "question_answered_icon": "https://thekardashians.b-cdn.net/the_family/icons/answered_question_the_family.svg",
-    //   "background": "https://thekardashians.b-cdn.net/the_family/background/the_family_background.png",
-
-        // this.backgroundImage = sortedCategories[0].background;
-        this.backgroundImage = GirlImage;
-        this.categoryName = sortedCategories[0].title;
-        this.categoryImage = sortedCategories[0].chosen_icon;
-        this.totalQuestionsCount = sortedCategories[0].questions.length;
+        const selectedCategory = sortedCategories[0];
+        let { background, title, chosen_icon, questions } = selectedCategory;
+        this.backgroundImage = background;
+        // this.backgroundImage = GirlImage;
+        this.categoryName = title;
+        this.categoryImage = chosen_icon;
+        this.totalQuestionsCount = questions.length;
         // this.backgroundImage = "https://www.imagediamond.com/blog/wp-content/uploads/2020/06/cartoon-boy-images-3-scaled.jpg";
     },
     methods: {
